14e707b631a837daa585e400e6d8a3d9afb7cef8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-03-10 15:17:54 +0100

    Update model:core to 1.0.0 (#122)

--- project/Versions.scala
+++ project/Versions.scala
@@ -27,6 +27,6 @@ object Versions {
   val SourceCodeVersion     = "0.2.1"
   val Specs2Version         = "4.8.3"
   val Stac4SVersion         = "0.0.3"
-  val SttpModelVersion      = "1.0.0-RC7"
+  val SttpModelVersion      = "1.0.0"
   val TapirVersion          = "0.12.21"
 }
