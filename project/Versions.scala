e10ff1e120af78c6ce0541950b3d68338b956f90
Scala Steward <me@scala-steward.org>
2020-04-06 00:07:15 +0200

    Update doobie-core, doobie-free, ... to 0.9.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,7 +7,7 @@ object Versions {
   val CirceFs2Version       = "0.13.0"
   val CirceVersion          = "0.13.0"
   val DeclineVersion        = "1.0.0"
-  val DoobieVersion         = "0.8.8"
+  val DoobieVersion         = "0.9.0"
   val EmojiVersion          = "1.2.1"
   val Flyway                = "6.3.2"
   val Fs2Version            = "2.3.0"
