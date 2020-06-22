158b5fcc71841347b18844943b2115c005b9442d
James Santucci <james.santucci@gmail.com>
2020-06-22 17:32:34 -0500

    remove debugging printlns

--- application/src/test/scala/com/azavea/franklin/extensions/validation/ValidationSpec.scala
+++ application/src/test/scala/com/azavea/franklin/extensions/validation/ValidationSpec.scala
@@ -52,9 +52,6 @@ class ValidationSpec extends Specification with ScalaCheck {
       .errors
       .toSet
 
-    println(s"Test is: ${test}")
-    println(s"Expectation is: ${expectation}")
-
     test == expectation
   }
 
