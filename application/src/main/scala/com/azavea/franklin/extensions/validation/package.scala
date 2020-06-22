6fd68e0526c6794c5111b201072777c46a527e6f
James Santucci <james.santucci@gmail.com>
2020-06-22 16:51:52 -0500

    don't blow up if no extensions passed

--- application/src/main/scala/com/azavea/franklin/extensions/validation/package.scala
+++ application/src/main/scala/com/azavea/franklin/extensions/validation/package.scala
@@ -1,34 +1,32 @@
 package com.azavea.franklin.extensions
 
-import com.azavea.stac4s.StacItem
-import com.azavea.stac4s.extensions.label.LabelItemExtension
+import cats.implicits._
+import com.azavea.stac4s.{StacItem, StacLink}
+import com.azavea.stac4s.extensions.label.{LabelItemExtension, LabelLinkExtension}
 import com.azavea.stac4s.extensions.layer.LayerItemExtension
-import com.azavea.stac4s.extensions.label.LabelLinkExtension
-import com.azavea.stac4s.StacLink
 
 import monocle.macros.GenLens
 
 package object validation {
 
   def getItemValidator(extensions: List[ExtensionName]): StacItem => StacItem = {
-    val checks = extensions map {
+    makeValidator(extensions map {
       case Label        => ItemValidator[LabelItemExtension].validate _
       case Layer        => ItemValidator[LayerItemExtension].validate _
       case Unchecked(_) => (x: StacItem) => x
-    }
-
-    checks reduce { _ compose _ }
+    })
   }
 
   def getLinkValidator(extensions: List[ExtensionName]): StacLink => StacLink = {
-    val checks = extensions map {
+    makeValidator(extensions map {
       case Label        => LinkValidator[LabelLinkExtension].validate _
       case Layer        => (x: StacLink) => x
       case Unchecked(_) => (x: StacLink) => x
-    }
-
-    checks reduce { _ compose _ }
+    })
   }
+
+  private def makeValidator[A](funcs: List[A => A]): A => A =
+    funcs.toNel map { _.reduce((f1: A => A, f2: A => A) => f1 compose f2) } getOrElse { x => x }
 
   val linksLens = GenLens[StacItem](_.links)
 }
