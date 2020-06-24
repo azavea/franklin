f8b2b3df2444bd63735e320357f020d1b80a7a15
James Santucci <james.santucci@gmail.com>
2020-06-24 13:18:24 -0500

    fixups from rebase

--- application/src/main/scala/com/azavea/franklin/api/services/CollectionItemsService.scala
+++ application/src/main/scala/com/azavea/franklin/api/services/CollectionItemsService.scala
@@ -3,7 +3,7 @@ package com.azavea.franklin.api.services
 import cats.data.NonEmptyList
 import cats.effect._
 import cats.implicits._
-import cats.{Applicative, Functor}
+import cats.Applicative
 import com.azavea.franklin.api.commands.ApiConfig
 import com.azavea.franklin.api.endpoints.CollectionItemEndpoints
 import com.azavea.franklin.api.implicits._
--- application/src/main/scala/com/azavea/franklin/extensions/validation/package.scala
+++ application/src/main/scala/com/azavea/franklin/extensions/validation/package.scala
@@ -1,9 +1,6 @@
 package com.azavea.franklin.extensions
 
-<<<<<<< HEAD
-=======
 import cats.Functor
->>>>>>> master
 import cats.implicits._
 import com.azavea.stac4s.extensions.label.{LabelItemExtension, LabelLinkExtension}
 import com.azavea.stac4s.extensions.layer.LayerItemExtension
@@ -12,21 +9,6 @@ import monocle.macros.GenLens
 
 package object validation {
 
-<<<<<<< HEAD
-  def getItemValidator(extensions: List[ExtensionName]): StacItem => StacItem = {
-    makeValidator(extensions map {
-      case Label        => ItemValidator[LabelItemExtension].validate _
-      case Layer        => ItemValidator[LayerItemExtension].validate _
-      case Unchecked(_) => (x: StacItem) => x
-    })
-  }
-
-  def getLinkValidator(extensions: List[ExtensionName]): StacLink => StacLink = {
-    makeValidator(extensions map {
-      case Label        => LinkValidator[LabelLinkExtension].validate _
-      case Layer        => (x: StacLink) => x
-      case Unchecked(_) => (x: StacLink) => x
-=======
   def getItemValidator(extensions: List[String]): StacItem => StacItem = {
     makeValidator(extensions map { s =>
       ExtensionName.fromString(s) match {
@@ -44,7 +26,6 @@ package object validation {
         case Layer        => (x: StacLink) => x
         case Unchecked(_) => (x: StacLink) => x
       }
->>>>>>> master
     })
   }
 
@@ -52,8 +33,6 @@ package object validation {
     funcs.toNel map { _.reduce((f1: A => A, f2: A => A) => f1 compose f2) } getOrElse { x => x }
 
   val linksLens = GenLens[StacItem](_.links)
-<<<<<<< HEAD
-=======
 
   def validateItemAndLinks(item: StacItem): StacItem = {
     val itemValidator = getItemValidator(item.stacExtensions)
@@ -61,5 +40,4 @@ package object validation {
 
     (linksLens.modify(Functor[List].lift(linkValidator)) compose itemValidator)(item)
   }
->>>>>>> master
 }
