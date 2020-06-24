bd55ca393476e6a4fba7a019f058b0f132026217
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-06-24 20:11:15 +0200

    Update monocle-core, monocle-macro to 2.0.5 (#290)

--- project/Versions.scala
+++ project/Versions.scala
@@ -20,7 +20,7 @@ object Versions {
   val Log4CatsVersion         = "1.1.1"
   val MagnoliaVersion         = "0.16.0"
   val MamlVersion             = "0.6.1"
-  val MonocleVersion          = "2.0.1"
+  val MonocleVersion          = "2.0.5"
   val Postgis                 = "2.5.0"
   val PureConfig              = "0.12.1"
   val Refined                 = "0.9.14"
