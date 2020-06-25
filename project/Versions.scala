f34ab43001c406f5a7c8e5179a4df775a8859b89
Scala Steward <me@scala-steward.org>
2020-06-25 06:07:48 +0200

    Update http4s-blaze-server, http4s-circe, ... to 0.21.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object Versions {
   val GeotrellisServerVersion = "4.2.0"
   val GuavaVersion            = "23.0"
   val HikariVersion           = "3.4.5"
-  val Http4sVersion           = "0.21.4"
+  val Http4sVersion           = "0.21.5"
   val JtsVersion              = "1.16.1"
   val Log4CatsVersion         = "1.1.1"
   val MagnoliaVersion         = "0.16.0"
