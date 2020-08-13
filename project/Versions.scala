bd83df6b0931d43a7bb4ec18d0484c4d4981d3c0
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-13 14:32:49 +0200

    Update specs2-core, specs2-scalacheck to 4.10.3 (#378)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -31,7 +31,7 @@ object Versions {
   val ShapelessVersion        = "2.3.3"
   val Slf4jVersion            = "1.7.30"
   val SourceCodeVersion       = "0.2.1"
-  val Specs2Version           = "4.10.2"
+  val Specs2Version           = "4.10.3"
   val SpireVersion            = "0.13.0"
   val Stac4SVersion           = "0.0.14"
   val SttpModelVersion        = "1.1.4"
