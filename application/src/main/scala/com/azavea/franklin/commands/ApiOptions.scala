8dade7b045eddc2313db47d6bd5538b9adaa1cd1
Orestis Herodotou <admin@digitaltopo.net>
2020-08-28 12:48:35 -0700

    Fix env var options types and formatting

--- application/src/main/scala/com/azavea/franklin/commands/ApiOptions.scala
+++ application/src/main/scala/com/azavea/franklin/commands/ApiOptions.scala
@@ -1,28 +1,30 @@
 package com.azavea.franklin.api.commands
 
+import cats.data.Validated
 import cats.implicits._
 import com.monovore.decline.Opts
 import com.monovore.decline.refined._
 import eu.timepit.refined.types.numeric.NonNegInt
 import eu.timepit.refined.types.numeric.PosInt
 
+import scala.language.postfixOps
+import scala.util.Try
+
 trait ApiOptions {
 
   private val portDefault      = PosInt(9090)
   private val externalPortHelp = s"Port users/clients hit for requests. Default: '$portDefault'."
 
-  val externalPort = Opts
-    .option[PosInt]("external-port", help = externalPortHelp) orElse
-    Opts.env[PosInt]("API_EXTERNAL_PORT", help = externalPortHelp) withDefault (portDefault)
+  val externalPort = Opts.option[PosInt]("external-port", help = externalPortHelp) orElse Opts
+    .env[PosInt]("API_EXTERNAL_PORT", help = externalPortHelp) withDefault (portDefault)
 
   private val internalPortHelp =
     s"Port server listens on, this will be different from 'external-port' when service is started behind a proxy. Default: '$portDefault'."
 
-  val internalPort = Opts
-    .option[PosInt](
-      "internal-port",
-      help = internalPortHelp
-    ) orElse Opts
+  val internalPort = Opts.option[PosInt](
+    "internal-port",
+    help = internalPortHelp
+  ) orElse Opts
     .env[PosInt]("API_INTERNAL_PORT", help = internalPortHelp) withDefault (portDefault)
 
   private val apiHostDefault = "localhost"
@@ -30,61 +32,69 @@ trait ApiOptions {
   private val apiHostHelp =
     s"Hostname Franklin is hosted it (e.g. localhost). Default: '$apiHostDefault'."
 
-  val apiHost = Opts
-    .option[String]("api-host", help = apiHostHelp) orElse
-    Opts.env[String]("API_HOST", help = apiHostHelp) withDefault (apiHostDefault)
+  val apiHost = Opts.option[String]("api-host", help = apiHostHelp) orElse Opts
+    .env[String]("API_HOST", help = apiHostHelp) withDefault (apiHostDefault)
 
   private val apiPathHelp = "Path component for root of Franklin instance (e.g. /stac/api)."
 
-  val apiPath = Opts
-    .option[String](
-      "api-path",
-      help = apiPathHelp
-    ) orElse
-    Opts.env[String]("API_PATH", help = apiPathHelp) orNone
+  val apiPath = (Opts.option[String](
+    "api-path",
+    help = apiPathHelp
+  ) orElse Opts.env[String]("API_PATH", help = apiPathHelp)) orNone
 
   private val apiSchemeDefault = "http"
 
   private val apiSchemeHelp =
     s"Scheme server is exposed to end users with. Default: '$apiSchemeDefault'."
 
-  val apiScheme =
-    Opts
-      .option[String]("api-scheme", help = apiSchemeHelp) orElse
-      Opts.env[String]("API_SCHEME", help = apiSchemeHelp) withDefault ("http")
-  validate("Scheme must be either 'http' or 'https'")(s => (s == "http" || s == "https"))
+  val apiScheme = (Opts
+    .option[String]("api-scheme", help = apiSchemeHelp) orElse Opts
+    .env[String]("API_SCHEME", help = apiSchemeHelp))
+    .withDefault("http")
+    .validate(
+      "Scheme must be either 'http' or 'https'"
+    )(s => (s == "http" || s == "https"))
 
   private val defaultLimitDefault = NonNegInt(30)
 
   private val defaultLimitHelp =
     s"Default limit for items returned in paginated responses. Default: '$defaultLimitDefault'."
 
-  private val defaultLimit =
-    Opts
-      .option[NonNegInt]("default-limit", help = defaultLimitHelp) orElse
-      Opts.env[NonNegInt]("API_DEFAULT_LIMIT", help = defaultLimitHelp) withDefault (defaultLimitDefault)
+  private val defaultLimit = Opts
+    .option[NonNegInt]("default-limit", help = defaultLimitHelp) orElse Opts
+    .env[NonNegInt]("API_DEFAULT_LIMIT", help = defaultLimitHelp) withDefault (defaultLimitDefault)
 
   private val enableTransactionsHelp =
     "Whether to respond to transaction requests, like adding or updating items. Default: 'false'."
 
-  private val enableTransactions =
-    Opts
-      .flag(
-        "with-transactions",
-        help = enableTransactionsHelp
-      ) orElse
-      Opts.env[Boolean]("API_WITH_TRANSACTIONS", help = enableTransactionsHelp) orFalse
+  private val enableTransactions = Opts
+    .flag(
+      "with-transactions",
+      help = enableTransactionsHelp
+    )
+    .orFalse orElse Opts
+    .env[String]("API_WITH_TRANSACTIONS", help = enableTransactionsHelp, metavar = "true||false")
+    .mapValidated { s =>
+      if (Try(s.toBoolean).getOrElse(false)) Validated.valid(s.toBoolean)
+      else Validated.invalidNel("Value must be Boolean")
+    } withDefault (false)
 
   private val enableTilesHelp = "Whether to include tile endpoints. Default: 'false'."
 
-  private val enableTiles = Opts.flag("with-tiles", help = enableTilesHelp) orElse
-    Opts.env[Boolean]("API_WITH_TILES", help = enableTilesHelp) orFalse
-
-  private val runMigrationsHelp = "Run migrations before the API server starts. Default: 'false'."
+  private val enableTiles = Opts
+    .flag(
+      "with-tiles",
+      help = enableTilesHelp
+    )
+    .orFalse orElse Opts
+    .env[String]("API_WITH_TILES", help = enableTilesHelp, metavar = "true||false")
+    .mapValidated { s =>
+      if (Try(s.toBoolean).getOrElse(false)) Validated.valid(s.toBoolean)
+      else Validated.invalidNel("Value must be Boolean")
+    } withDefault (false)
 
   private val runMigrations =
-    Opts.flag("run-migrations", help = runMigrationsHelp) orElse
-      Opts.env[Boolean]("API_RUN_MIGRATIONS", help = runMigrationsHelp) orFalse
+    Opts.flag("run-migrations", "Run migrations before the API server starts").orFalse
 
   val apiConfig: Opts[ApiConfig] = (
     externalPort,
