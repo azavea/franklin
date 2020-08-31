c635da246090f5a5d90204e2bc8b27d86bf0958d
Scala Steward <me@scala-steward.org>
2020-08-31 04:04:57 +0200

    Update decline, decline-refined to 1.3.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,7 +7,7 @@ object Versions {
   val CatsVersion             = "2.1.1"
   val CirceFs2Version         = "0.13.0"
   val CirceVersion            = "0.13.0"
-  val DeclineVersion          = "1.2.0"
+  val DeclineVersion          = "1.3.0"
   val DoobieVersion           = "0.9.0"
   val EmojiVersion            = "1.2.1"
   val Flyway                  = "6.5.5"
