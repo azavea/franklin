6e0c828ed57fa5389344b307ca9d48114beacd80
Scala Steward <me@scala-steward.org>
2020-11-24 20:10:29 +0100

    Update fs2-core to 2.4.6

--- project/Versions.scala
+++ project/Versions.scala
@@ -11,7 +11,7 @@ object Versions {
   val DoobieVersion           = "0.9.2"
   val EmojiVersion            = "1.2.1"
   val Flyway                  = "7.2.1"
-  val Fs2Version              = "2.4.5"
+  val Fs2Version              = "2.4.6"
   val GeoTrellisVersion       = "3.5.1"
   val GeotrellisServerVersion = "4.2.0"
   val GuavaVersion            = "30.0-jre"
