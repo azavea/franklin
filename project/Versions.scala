d3928db9b39090faa5a91ff4fe0f0a1645b8d189
Scala Steward <me@scala-steward.org>
2021-02-24 00:07:19 +0100

    Update typename to 0.1.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -39,5 +39,5 @@ object Versions {
   val SttpClientVersion       = "2.2.9"
   val SttpModelVersion        = "1.1.4"
   val TapirVersion            = "0.16.16"
-  val TypenameVersion         = "0.1.3"
+  val TypenameVersion         = "0.1.5"
 }
