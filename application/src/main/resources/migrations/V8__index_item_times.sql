50598120362364ced7178edc84e75df0e2ba6be6
James Santucci <james.santucci@gmail.com>
2021-03-22 14:43:10 -0600

    Performance -- separately index datetime fields, count item results in parallel (#630)

---
+++ application/src/main/resources/migrations/V8__index_item_times.sql
@@ -0,0 +1,67 @@
+-- this index was referring to the top-level item object, which doesn't
+-- help with searches into the item's `properties` field
+DROP INDEX IF EXISTS collection_item_json_idx;
+
+ALTER TABLE
+    collection_items
+ADD
+    COLUMN start_datetime timestamp with time zone;
+
+ALTER TABLE
+    collection_items
+ADD
+    COLUMN end_datetime timestamp with time zone;
+
+ALTER TABLE
+    collection_items
+ADD
+    COLUMN datetime timestamp with time zone;
+
+-- in a testing setting, this temp table hasn't disappeared
+-- by the time migration 8 needs to be applied 🤦🏻‍♂️
+DROP TABLE IF EXISTS collection_items_tmp;
+
+CREATE TEMP TABLE collection_items_tmp AS (
+    SELECT
+        id,
+        geom,
+        item,
+        serial_id,
+        created_at,
+        collection,
+        (
+            (item -> 'properties' ->> 'start_datetime') :: timestamp with time zone
+        ) start_datetime,
+        (
+            (item -> 'properties' ->> 'end_datetime') :: timestamp with time zone
+        ) end_datetime,
+        (
+            (item -> 'properties' ->> 'datetime') :: timestamp with time zone
+        ) datetime
+    FROM
+        collection_items
+);
+
+TRUNCATE TABLE collection_items;
+
+-- this index replaces the former collection_item_json_idx but indexing into properties instead of
+-- indexing the top-level of the item
+CREATE INDEX IF NOT EXISTS collection_items_properties_idx ON collection_items USING gin ((item -> 'properties'));
+
+CREATE INDEX IF NOT EXISTS collection_items_start_datetime_idx ON collection_items (start_datetime);
+
+CREATE INDEX IF NOT EXISTS collection_items_end_datetime_idx ON collection_items (end_datetime);
+
+CREATE INDEX IF NOT EXISTS collection_items_datetime_idx ON collection_items (datetime);
+
+INSERT INTO
+    collection_items (
+        select
+            *
+        from
+            collection_items_tmp
+    );
+
+-- this cleanup will mean that future migrations won't have to drop
+-- the table twice to be applicable in test settings, just once at the end
+DROP TABLE IF EXISTS collection_items_tmp;
