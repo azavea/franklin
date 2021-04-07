28550e1a835681f8f3d0e4a5c37870ba5289f574
Scala Steward <me@scala-steward.org>
2021-04-08 00:11:48 +0200

    Update doobie-core, doobie-free, ... to 0.12.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,7 +8,7 @@ object Versions {
   val CirceFs2Version         = "0.13.0"
   val CirceVersion            = "0.13.0"
   val DeclineVersion          = "2.0.0"
-  val DoobieVersion           = "0.10.0"
+  val DoobieVersion           = "0.12.1"
   val EmojiVersion            = "1.2.2"
   val Flyway                  = "7.7.2"
   val Fs2Version              = "2.5.0"
