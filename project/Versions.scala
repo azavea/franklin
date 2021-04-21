57bac92b9d65fc14a17debc12a19cf66fce9428f
Scala Steward <me@scala-steward.org>
2021-04-21 22:09:15 +0200

    Update model:core to 1.4.4

--- project/Versions.scala
+++ project/Versions.scala
@@ -39,7 +39,7 @@ object Versions {
   val Stac4SVersion           = "0.2.1"
   val SttpClientVersion       = "2.2.9"
   val SttpShared              = "1.1.0"
-  val SttpModelVersion        = "1.4.3"
+  val SttpModelVersion        = "1.4.4"
   val TapirVersion            = "0.17.12"
   val TapirOpenAPIVersion     = "0.17.12"
   val ThreeTenExtra           = "1.6.0"
