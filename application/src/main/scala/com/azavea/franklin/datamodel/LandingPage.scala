f0e8f9bd8135bab81f2a6c1c3f0eb545ce9de405
James Santucci <james.santucci@gmail.com>
2021-04-28 09:41:22 -0600

    make landing page match current spec

--- application/src/main/scala/com/azavea/franklin/datamodel/LandingPage.scala
+++ application/src/main/scala/com/azavea/franklin/datamodel/LandingPage.scala
@@ -2,9 +2,19 @@ package com.azavea.franklin.datamodel
 
 import com.azavea.stac4s.StacLinkType
 import eu.timepit.refined.types.string.NonEmptyString
+import io.circe.Encoder
 import io.circe.generic.semiauto._
+import com.azavea.stac4s.StacLink
 
-case class LandingPage(title: NonEmptyString, description: NonEmptyString, links: List[Link]) {
+case class LandingPage(
+    stacVersion: String,
+    stacExtensions: List[String],
+    title: Option[String],
+    id: NonEmptyString,
+    description: NonEmptyString,
+    links: List[Link],
+    conformsTo: List[NonEmptyString]
+) {
 
   lazy val dataLinks = links.filter(_.rel == StacLinkType.Data)
 
@@ -13,6 +23,27 @@ case class LandingPage(title: NonEmptyString, description: NonEmptyString, links
 }
 
 object LandingPage {
-  implicit val landingPageEncoder = deriveEncoder[LandingPage]
+
+  implicit val landingPageEncoder: Encoder[LandingPage] = Encoder.forProduct8(
+    "type",
+    "stac_version",
+    "stac_extensions",
+    "title",
+    "id",
+    "description",
+    "links",
+    "conformsTo"
+  )(landingPage =>
+    (
+      "Catalog",
+      landingPage.stacVersion,
+      landingPage.stacExtensions,
+      landingPage.title,
+      landingPage.id,
+      landingPage.description,
+      landingPage.links,
+      landingPage.conformsTo
+    )
+  )
   implicit val landingPageDecoder = deriveDecoder[LandingPage]
 }
