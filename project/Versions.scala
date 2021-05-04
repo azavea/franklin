5cec7b166312bb1114ca7a299baffe852e13a0b3
Scala Steward <me@scala-steward.org>
2021-05-04 22:41:49 +0200

    Update shared:core, shared:fs2 to 1.2.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -38,7 +38,7 @@ object Versions {
   val SpireVersion            = "0.13.0"
   val Stac4SVersion           = "0.2.3"
   val SttpClientVersion       = "2.2.9"
-  val SttpShared              = "1.1.1"
+  val SttpShared              = "1.2.3"
   val SttpModelVersion        = "1.4.6"
   val TapirVersion            = "0.17.12"
   val TapirOpenAPIVersion     = "0.17.12"
