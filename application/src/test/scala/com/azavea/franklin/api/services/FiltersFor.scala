77a7fc63abbe9c2f12eaeeb33ec7d679ba670558
James Santucci <james.santucci@gmail.com>
2021-05-12 17:17:48 -0600

    lint

--- application/src/test/scala/com/azavea/franklin/api/services/FiltersFor.scala
+++ application/src/test/scala/com/azavea/franklin/api/services/FiltersFor.scala
@@ -1,8 +1,8 @@
 package com.azavea.franklin.api.services
 
-import cats.{Monoid, Semigroup}
 import cats.data.NonEmptyList
 import cats.syntax.all._
+import cats.{Monoid, Semigroup}
 import com.azavea.franklin.database.SearchFilters
 import com.azavea.stac4s.jvmTypes.TemporalExtent
 import com.azavea.stac4s.{ItemDatetime, StacCollection, StacItem, TwoDimBbox}
