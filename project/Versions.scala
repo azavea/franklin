9bad0c1fc29806084d533fdf092a1f479a2380d1
Scala Steward <me@scala-steward.org>
2021-05-17 12:08:44 +0200

    Update jts-core to 1.18.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -18,7 +18,7 @@ object Versions {
   val GuavaVersion            = "30.1.1-jre"
   val HikariVersion           = "4.0.3"
   val Http4sVersion           = "0.21.23"
-  val JtsVersion              = "1.16.1"
+  val JtsVersion              = "1.18.1"
   val LogbackVersion          = "1.2.3"
   val Log4CatsVersion         = "1.1.1"
   val MagnoliaVersion         = "0.17.0"
