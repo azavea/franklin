8529fe611cda4e4e14a4a82fd2ac6d11061d31b0
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-06-18 09:41:09 +0200

    Update slf4j-api, slf4j-simple to 1.7.31 (#787)

--- project/Versions.scala
+++ project/Versions.scala
@@ -33,7 +33,7 @@ object Versions {
   val ScalaReflectVersion     = "2.12.10"
   val ScapegoatVersion        = "1.3.11"
   val ShapelessVersion        = "2.3.7"
-  val Slf4jVersion            = "1.7.30"
+  val Slf4jVersion            = "1.7.31"
   val Specs2Version           = "4.12.1"
   val SpireVersion            = "0.13.0"
   val Stac4SVersion           = "0.5.0"
