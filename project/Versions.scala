a8a3eddc45972aee0b85a9d2131549f7e01cb483
Scala Steward <me@scala-steward.org>
2021-06-25 12:09:44 +0200

    Update specs2-core, specs2-scalacheck to 4.12.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -34,7 +34,7 @@ object Versions {
   val ScapegoatVersion        = "1.3.11"
   val ShapelessVersion        = "2.3.7"
   val Slf4jVersion            = "1.7.31"
-  val Specs2Version           = "4.12.1"
+  val Specs2Version           = "4.12.2"
   val SpireVersion            = "0.13.0"
   val Stac4SVersion           = "0.5.0"
   val SttpClientVersion       = "2.2.9"
