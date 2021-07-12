fe7863e60edb5a54d2508a096d2c85656563796c
James Sanntucci <james.santucci@gmail.com>
2021-07-12 16:10:37 -0600

    testing lint

--- application/src/test/scala/com/azavea/franklin/datamodel/SerDeSpec.scala
+++ application/src/test/scala/com/azavea/franklin/datamodel/SerDeSpec.scala
@@ -2,20 +2,20 @@ package com.azavea.franklin.datamodel
 
 import cats.effect.IO
 import cats.syntax.all._
+import com.azavea.franklin.Generators
 import com.azavea.franklin.api.TestImplicits
 import com.azavea.stac4s.StacItem
 import com.azavea.stac4s.syntax._
 import com.azavea.stac4s.testing.JvmInstances._
 import com.azavea.stac4s.testing._
 import eu.timepit.refined.types.string.NonEmptyString
+import io.circe.testing.{ArbitraryInstances, CodecTests}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.scalacheck.Checkers
 import org.specs2.{ScalaCheck, Specification}
+import org.typelevel.discipline.scalatest.FunSuiteDiscipline
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import org.scalatest.funsuite.AnyFunSuite
-import com.azavea.franklin.Generators
-import org.typelevel.discipline.scalatest.FunSuiteDiscipline
-import org.scalatestplus.scalacheck.Checkers
-import io.circe.testing.{ArbitraryInstances, CodecTests}
 
 class SerDeSpec
     extends AnyFunSuite
