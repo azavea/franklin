3e7aa72e26d0e70e7ebfd060a899ab948ad66af9
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-07-13 12:24:52 -0400

    Get rid of an extra flatMap with an extra Sync[F] wrapper

--- application/src/main/scala/com/azavea/franklin/tile/CogAssetNodeImplicits.scala
+++ application/src/main/scala/com/azavea/franklin/tile/CogAssetNodeImplicits.scala
@@ -15,36 +15,28 @@ object CogAssetNodeImplicits extends TileUtil {
 
   implicit def cogAssetNodeTmsReification[F[_]: Sync]: TmsReification[F, CogAssetNode] = {
     (self, _) => (z: Int, x: Int, y: Int) =>
-      {
-        def fetch(xCoord: Int, yCoord: Int): F[Raster[MultibandTile]] = {
-          self.fetchTile(z, xCoord, yCoord, WebMercator).flatMap(a => Sync[F].delay(a))
-        }
-
-        fetch(x, y).map { tile =>
-          val extent = tmsLevels(z).mapTransform.keyToExtent(x, y)
-          ProjectedRaster(tile.tile, extent, WebMercator)
-        }
+      self.fetchTile(z, x, y, WebMercator).map { tile =>
+        val extent = tmsLevels(z).mapTransform.keyToExtent(x, y)
+        ProjectedRaster(tile.tile, extent, WebMercator)
       }
   }
 
   implicit def cogAssetNodeExtentReification[F[_]: Sync]: ExtentReification[F, CogAssetNode] = {
     self => (extent: Extent, ocs: Option[CellSize]) =>
-      {
-        self.getRasterSource map { rs =>
-          val cs = ocs.getOrElse(rs.cellSize)
-          rs.resample(
-              TargetRegion(new GridExtent[Long](extent, cs)),
-              NearestNeighbor,
-              AutoHigherResolution
+      self.getRasterSource map { rs =>
+        val cs = ocs.getOrElse(rs.cellSize)
+        rs.resample(
+            TargetRegion(new GridExtent[Long](extent, cs)),
+            NearestNeighbor,
+            AutoHigherResolution
+          )
+          .read(extent)
+          .map(ProjectedRaster(_, WebMercator)) match {
+          case Some(mbt) => mbt
+          case _ =>
+            throw new Exception(
+              s"No tile available for RasterExtent: ${RasterExtent(extent, cs)}"
             )
-            .read(extent)
-            .map(ProjectedRaster(_, WebMercator)) match {
-            case Some(mbt) => mbt
-            case _ =>
-              throw new Exception(
-                s"No tile available for RasterExtent: ${RasterExtent(extent, cs)}"
-              )
-          }
         }
       }
   }
