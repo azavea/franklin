a69f29f4b3b38269c856e4fb70380219965d71fe
James Santucci <james.santucci@gmail.com>
2021-07-29 17:08:44 -0600

    Improve BadResponse messages (#848)
    * remove twirl stuff
    
    * wip -- better, but only prints circe rep of first error
    
    * Wire up accumulatingJsonBody everywhere
    
    * remove string interpolation
    
    * lint
    
    * Upgrade stac4s
    
    * Improve message
    
    * lint

--- application/src/main/scala/com/azavea/franklin/api/Server.scala
+++ application/src/main/scala/com/azavea/franklin/api/Server.scala
@@ -54,8 +54,6 @@ object Server extends IOApp.WithContext {
       )
       .map(ExecutionContext.fromExecutor _)
 
-  implicit val serverOptions = ServerOptions.defaultServerOptions[IO]
-
   private val banner: List[String] =
     """
    $$$$$$$$$
@@ -70,6 +68,8 @@ $$$$$$        $$ |   $$ |     $$    $$ |$$ |  $$ |$$ | $$  |$$ |$$ |$$ |  $$ |
 $$$$$         $$/    $$/       $$$$$$$/ $$/   $$/ $$/   $$/ $$/ $$/ $$/   $$/
 $$$$
 """.split("\n").toList
+
+  implicit val serverOptions = ServerOptions.defaultServerOptions[IO]
 
   private def createServer(
       apiConfig: ApiConfig,
--- application/src/main/scala/com/azavea/franklin/api/endpoints/package.scala
+++ application/src/main/scala/com/azavea/franklin/api/endpoints/package.scala
@@ -1,6 +1,10 @@
 package com.azavea.franklin.api
 
-import cats.data.NonEmptyList
+import cats.data.{NonEmptyList, Validated}
+import io.circe.syntax._
+import io.circe.{CursorOp, Decoder, DecodingFailure, Encoder, Json, ParsingFailure}
+import sttp.tapir.Codec.JsonCodec
+import sttp.tapir.SchemaType._
 import sttp.tapir._
 
 package object endpoints {
@@ -16,4 +20,57 @@ package object endpoints {
 
   def baseFor(prefix: Option[String], basePath: EndpointInput[Unit]) =
     prefix.fold(basePath)(prefix => prependApiPath(prefix, basePath))
+
+  private def circeErrorToFailure(
+      failure: io.circe.Error
+  ): DecodeResult.Error.JsonError = failure match {
+    case DecodingFailure(err, hist) =>
+      val path   = CursorOp.opsToPath(hist)
+      val fields = path.split("\\.").toList.filter(_.nonEmpty).map(FieldName.apply)
+      DecodeResult.Error.JsonError(err, fields)
+    case ParsingFailure(message, _) =>
+      DecodeResult.Error.JsonError(message, path = List.empty)
+  }
+
+  private def errsToMessage(
+      errs: NonEmptyList[io.circe.Error]
+  ): Exception = {
+    val errMessage = Map[String, Json](
+      "parseFailures" -> errs
+        .collect({
+          case ParsingFailure(_, underlying) =>
+            s"Failed to parse input as JSON: ${underlying.getMessage}"
+        })
+        .asJson,
+      "decodingFailures" -> errs
+        .collect({
+          case DecodingFailure("Attempt to decode value on failed cursor", history) =>
+            s"${CursorOp.opsToPath(history)} was missing but required"
+          case DecodingFailure(s, history) =>
+            List(
+              s"${CursorOp.opsToPath(history)} had the wrong type.",
+              s"The error was: $s.",
+              "If the error is a plain type like 'String', the value must be that type, for example a String instead of an Int.",
+              "If the error says 'Predicate failed: ___', then the stated predicate must succeed for the value to be valid."
+            ).mkString(" ")
+
+        })
+        .asJson
+    )
+    new Exception(errMessage.asJson.spaces2)
+  }
+
+  private def circeCodec[T: Encoder: Decoder: Schema]: JsonCodec[T] =
+    sttp.tapir.Codec.json[T] { s =>
+      io.circe.parser.decodeAccumulating[T](s) match {
+        case Validated.Invalid(errs) =>
+          DecodeResult.Error(s, DecodeResult.Error.JsonDecodeException(errs.toList map {
+            circeErrorToFailure
+          }, errsToMessage(errs)))
+        case Validated.Valid(v) => DecodeResult.Value(v)
+      }
+    } { t => t.asJson.noSpaces }
+
+  def accumulatingJsonBody[T: Encoder: Decoder: Schema]: EndpointIO.Body[String, T] =
+    anyFromUtf8StringBody(circeCodec[T])
 }
