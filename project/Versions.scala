a5f5630792b1aa88b3be525aeb0488b907ab1bea
Scala Steward <me@scala-steward.org>
2021-10-12 00:05:44 +0200

    Update sdk-core to 2.17.57

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@
 object Versions {
   val AsyncHttpClientVersion = "2.12.3"
   val AWSVersion             = "1.11.751"
-  val AWSSdk2Version         = "2.17.56"
+  val AWSSdk2Version         = "2.17.57"
   val CatsEffectVersion      = "2.5.4"
   val CatsScalacheckVersion  = "0.3.1"
   val CatsVersion            = "2.6.1"
