38bb218376af065f0b40ce4615103cd20845651a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-11-10 20:15:25 +0100

    Update flyway-core to 8.0.4 (#1036)

--- project/Versions.scala
+++ project/Versions.scala
@@ -13,7 +13,7 @@ object Versions {
   val DisciplineScalatest    = "2.1.5"
   val DoobieVersion          = "0.13.4"
   val EmojiVersion           = "1.2.3"
-  val Flyway                 = "8.0.3"
+  val Flyway                 = "8.0.4"
   val Fs2Version             = "2.5.10"
   val GeoTrellisVersion      = "3.6.0"
   val GuavaVersion           = "31.0.1-jre"
