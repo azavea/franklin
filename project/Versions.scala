0347d09d89d5499092d374e01dca5baa449a2211
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-12-21 22:45:08 +0100

    Update model:core to 1.4.20 (#1090)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -40,7 +40,7 @@ object Versions {
   val Stac4SVersion          = "0.6.2"
   val SttpClientVersion      = "2.2.10"
   val SttpShared             = "1.3.0"
-  val SttpModelVersion       = "1.4.19"
+  val SttpModelVersion       = "1.4.20"
   val TapirVersion           = "0.17.20"
   val TapirOpenAPIVersion    = "0.17.20"
   val ThreeTenExtra          = "1.7.0"
