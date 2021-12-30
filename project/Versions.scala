738d2914829c3143625d96ef12c37b6498cb31eb
Scala Steward <me@scala-steward.org>
2021-12-30 20:42:35 +0100

    Update shared:core, shared:fs2-ce2 to 1.3.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -39,7 +39,7 @@ object Versions {
   val Specs2Version          = "4.13.1"
   val Stac4SVersion          = "0.6.2"
   val SttpClientVersion      = "2.2.10"
-  val SttpShared             = "1.3.0"
+  val SttpShared             = "1.3.1"
   val SttpModelVersion       = "1.4.20"
   val TapirVersion           = "0.17.20"
   val TapirOpenAPIVersion    = "0.17.20"
