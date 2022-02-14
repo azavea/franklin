0535f181fd3f29bd6d4b4526972e4690bcf1ad50
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-02-15 00:21:11 +0100

    Update specs2-core, specs2-scalacheck to 4.13.3 (#1138)

--- project/Versions.scala
+++ project/Versions.scala
@@ -36,7 +36,7 @@ object Versions {
   val ScapegoatVersion       = "1.4.11"
   val ShapelessVersion       = "2.3.8"
   val Slf4jVersion           = "1.7.36"
-  val Specs2Version          = "4.13.2"
+  val Specs2Version          = "4.13.3"
   val Stac4SVersion          = "0.6.2"
   val SttpClientVersion      = "2.3.0"
   val SttpShared             = "1.3.2"
