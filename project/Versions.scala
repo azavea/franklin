b77007a22a0a4e4c958f7b4e6825939501bd7a98
Scala Steward <me@scala-steward.org>
2022-03-14 18:08:06 +0100

    Update fs2-core to 3.2.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -18,7 +18,7 @@ object Versions {
   val DoobieVersion          = "0.13.4"
   val EmojiVersion           = "1.2.3"
   val Flyway                 = "8.5.3"
-  val Fs2Version             = "2.5.10"
+  val Fs2Version             = "3.2.5"
   val GeoTrellisVersion      = "3.6.0"
   val GuavaVersion           = "31.1-jre"
   val HikariVersion          = "4.0.3"
