b8f9709fab251e9186e464c010eebf408f8f4663
Scala Steward <me@scala-steward.org>
2022-03-28 00:12:28 +0200

    Revert commit(s) bd7297d

--- project/Versions.scala
+++ project/Versions.scala
@@ -33,7 +33,7 @@ object Versions {
   val Refined                = "0.9.28"
   val ScalacacheVersion      = "0.28.0"
   val ScalacheckVersion      = "1.15.4"
-  val ScapegoatVersion       = "1.4.12"
+  val ScapegoatVersion       = "1.4.11"
   val ShapelessVersion       = "2.3.8"
   val Slf4jVersion           = "1.7.36"
   val Specs2Version          = "4.13.2"
