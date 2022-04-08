8d74b4bdc577cd79322c56a1009a8e4e70f5ac82
Scala Steward <me@scala-steward.org>
2022-04-08 10:08:47 +0200

    Update scalacheck to 1.16.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -32,7 +32,7 @@ object Versions {
   val PureConfig             = "0.12.1"
   val Refined                = "0.9.28"
   val ScalacacheVersion      = "0.28.0"
-  val ScalacheckVersion      = "1.15.4"
+  val ScalacheckVersion      = "1.16.0"
   val ScapegoatVersion       = "1.4.11"
   val ShapelessVersion       = "2.3.9"
   val Slf4jVersion           = "1.7.36"
