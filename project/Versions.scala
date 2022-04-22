533a764a66bb65f6fd04f872b84d60e43ec303e2
Scala Steward <me@scala-steward.org>
2022-04-22 20:05:17 +0200

    Revert commit(s) 9ac4d1e

--- project/Versions.scala
+++ project/Versions.scala
@@ -19,7 +19,7 @@ object Versions {
   val EmojiVersion           = "1.2.3"
   val Flyway                 = "8.5.5"
   val Fs2Version             = "2.5.10"
-  val GeoTrellisVersion      = "3.6.2"
+  val GeoTrellisVersion      = "3.6.0"
   val GuavaVersion           = "31.1-jre"
   val HikariVersion          = "4.0.3"
   val Http4sVersion          = "0.21.33"
