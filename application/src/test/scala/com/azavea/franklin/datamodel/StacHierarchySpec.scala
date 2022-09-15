812aec5cf1370b9a79799fcc02b430adb5e1acf3
Nathan Zimmerman <npzimmerman@gmail.com>
2022-09-15 09:45:20 -0500

    Lint tests

--- application/src/test/scala/com/azavea/franklin/datamodel/StacHierarchySpec.scala
+++ application/src/test/scala/com/azavea/franklin/datamodel/StacHierarchySpec.scala
@@ -1,7 +1,6 @@
 package com.azavea.franklin.datamodel
 
 import com.azavea.franklin.datamodel.hierarchy._
-
 import io.circe._
 import io.circe.parser._
 import io.circe.syntax._
@@ -11,24 +10,34 @@ import org.scalatest.matchers.should.Matchers._
 
 class StacHierarchySpec extends AnyFlatSpec {
 
-    val hierarchy = RootNode(
-      List(
-        CollectionNode("joplin"),
-        CatalogNode("joplin-test", None, "A test of catalogs",
-          List(
-            CatalogNode("sub-joplin", None, "A more serious test",
-              List(
-                CollectionNode("joplin")
-              ),
-              List()
+  val hierarchy = RootNode(
+    List(
+      CollectionNode("joplin"),
+      CatalogNode(
+        "joplin-test",
+        None,
+        "A test of catalogs",
+        List(
+          CatalogNode(
+            "sub-joplin",
+            None,
+            "A more serious test",
+            List(
+              CollectionNode("joplin")
             ),
-            CollectionNode("joplin")
+            List()
           ),
-          List(ItemPath("joplin", "fe916452-ba6f-4631-9154-c249924a122d"))
-      )),
-      List(ItemPath("joplin", "al_m_3008506_nw_16_060_20191118_20200114"), ItemPath("joplin", "fe916452-ba6f-4631-9154-c249924a122d"))
+          CollectionNode("joplin")
+        ),
+        List(ItemPath("joplin", "fe916452-ba6f-4631-9154-c249924a122d"))
+      )
+    ),
+    List(
+      ItemPath("joplin", "al_m_3008506_nw_16_060_20191118_20200114"),
+      ItemPath("joplin", "fe916452-ba6f-4631-9154-c249924a122d")
     )
-  
+  )
+
   "A hierarchy" should "roundtrip serialize and deserialize" in {
     hierarchy.asJson.as[StacHierarchy] shouldBe ('right)
   }
@@ -41,8 +50,12 @@ class StacHierarchySpec extends AnyFlatSpec {
   }
 
   "A hierarchy" should "automatically register paths to nodes upon deserialization" in {
-    hierarchy.asJson.as[StacHierarchy].right.get
-      .children.find({ child =>
+    hierarchy.asJson
+      .as[StacHierarchy]
+      .right
+      .get
+      .children
+      .find({ child =>
         child.path.length == 1 && child.path.head == "joplin-test"
       }) shouldBe ('defined)
   }
