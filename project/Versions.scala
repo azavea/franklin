fb468d2710a075b02024ac09ab8e333ca2130589
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-10-17 15:54:28 +0200

    Update slf4j-api, slf4j-simple to 2.0.3 (#1303)

--- project/Versions.scala
+++ project/Versions.scala
@@ -34,7 +34,7 @@ object Versions {
   val ScalacheckVersion      = "1.16.0"
   val ScapegoatVersion       = "1.4.11"
   val ShapelessVersion       = "2.3.9"
-  val Slf4jVersion           = "1.7.36"
+  val Slf4jVersion           = "2.0.3"
   val Specs2Version          = "4.14.1"
   val Stac4SVersion          = "0.8.1"
   val SttpClientVersion      = "2.3.0"
