cf0e4a2c0b8ee52d2fae73e828b28005171930d2
Scala Steward <scala_steward@virtuslab.com>
2022-10-19 14:09:44 +0000

    Revert commit(s) d851cdf

--- project/Versions.scala
+++ project/Versions.scala
@@ -35,7 +35,7 @@ object Versions {
   val ScapegoatVersion       = "1.4.11"
   val ShapelessVersion       = "2.3.9"
   val Slf4jVersion           = "1.7.36"
-  val Specs2Version          = "4.17.0"
+  val Specs2Version          = "4.14.1"
   val Stac4SVersion          = "0.8.0"
   val SttpClientVersion      = "2.3.0"
   val SttpShared             = "1.3.5"
