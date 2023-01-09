b1fd0830c09f6cab07ec1081f86d2f3b30ded17d
Scala Steward <scala_steward@virtuslab.com>
2023-01-09 14:25:44 +0000

    Revert commit(s) c2b2278

--- project/Versions.scala
+++ project/Versions.scala
@@ -21,7 +21,7 @@ object Versions {
   val Fs2Version             = "2.5.10"
   val GeoTrellisVersion      = "3.6.0"
   val GuavaVersion           = "31.1-jre"
-  val HikariVersion          = "5.0.1"
+  val HikariVersion          = "4.0.3"
   val Http4sVersion          = "0.21.33"
   val JtsVersion             = "1.16.1"
   val LogbackVersion         = "1.2.5"
