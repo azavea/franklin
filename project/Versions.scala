d052e262e7329e9261a2c537094313cd0a2f888c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-04-15 15:17:22 +0200

    Update os-lib to 0.10.0 (#1640)

--- project/Versions.scala
+++ project/Versions.scala
@@ -27,7 +27,7 @@ object Versions {
   val Log4CatsVersion        = "1.1.1"
   val MagnoliaVersion        = "0.17.0"
   val MonocleVersion         = "2.1.0"
-  val OsLib                  = "0.9.3"
+  val OsLib                  = "0.10.0"
   val Postgis                = "2.5.1"
   val PureConfig             = "0.12.1"
   val Refined                = "0.9.29"
