d499cb0db33d03086da7c7c6fc2ae29042ba0b06
Scala Steward <scala_steward@virtuslab.com>
2024-07-14 12:58:37 +0000

    Update stac4s:core, stac4s:testing to 0.9.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -36,7 +36,7 @@ object Versions {
   val ShapelessVersion       = "2.3.12"
   val Slf4jVersion           = "2.0.13"
   val Specs2Version          = "4.20.8"
-  val Stac4SVersion          = "0.8.1"
+  val Stac4SVersion          = "0.9.1"
   val SttpClientVersion      = "2.3.0"
   val SttpShared             = "1.3.19"
   val SttpModelVersion       = "1.4.26"
