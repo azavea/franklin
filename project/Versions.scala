b3d6402d72adb65a8f025c8bbb0f097dcabc27a3
Scala Steward <scala_steward@virtuslab.com>
2024-09-26 13:09:43 +0000

    Update http4s-circe, http4s-core, ... to 0.23.28

--- project/Versions.scala
+++ project/Versions.scala
@@ -21,7 +21,7 @@ object Versions {
   val GeoTrellisVersion      = "3.6.3"
   val GuavaVersion           = "33.3.0-jre"
   val HikariVersion          = "6.0.0"
-  val Http4sVersion          = "0.21.34"
+  val Http4sVersion          = "0.23.28"
   val JtsVersion             = "1.18.1"
   val LogbackVersion         = "1.2.5"
   val Log4CatsVersion        = "1.1.1"
