922e2d405056774a7d7e011e4165e176d24d4f58
Scala Steward <scala_steward@virtuslab.com>
2024-11-13 14:45:23 +0000

    Update shared:core, shared:fs2-ce2 to 1.4.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -38,7 +38,7 @@ object Versions {
   val Specs2Version          = "4.20.9"
   val Stac4SVersion          = "0.8.1"
   val SttpClientVersion      = "2.3.0"
-  val SttpShared             = "1.3.22"
+  val SttpShared             = "1.4.0"
   val SttpModelVersion       = "1.4.26"
   val TapirVersion           = "0.17.20"
   val TapirOpenAPIVersion    = "0.17.20"
